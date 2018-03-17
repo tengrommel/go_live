71ba2435ebb7b49db01eb725de2eb9bb793028d9
tengrommel <wwwsousoucom@126.com>
2018-03-18 01:44:51 +0800

    stringSlice

---
+++ interface/03/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"io/ioutil"
+	"fmt"
+)
+
+func main() {
+	res, err := http.Get("http://www-01.sil.org/linguistics/wordlists/english/wordlist/wordsEn.txt")
+	if err != nil{
+		log.Fatalln(err)
+	}
+
+	bs, _ := ioutil.ReadAll(res.Body)
+	str := string(bs)
+	fmt.Println(str)
+}
