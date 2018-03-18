0689f3a009aa9de92f835f66861047e52230cbc9
tengrommel <wwwsousoucom@126.com>
2018-03-19 00:55:58 +0800

    http

---
+++ go_job/web/02_understanding-net-http/02_listenAndServe/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+type hotdog int
+
+func (m hotdog)ServeHTTP(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprintln(w, "Any code you want in this func")
+}
+
+
+func main() {
+	var d hotdog
+	http.ListenAndServe(":8080", d)
+}
