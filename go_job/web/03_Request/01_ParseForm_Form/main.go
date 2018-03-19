c51e687e1e5e7a215e73a87c56cd761030a4f4c0
tengrommel <wwwsousoucom@126.com>
2018-03-19 11:11:28 +0800

    http-handler

---
+++ go_job/web/03_Request/01_ParseForm_Form/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"html/template"
+)
+
+type hotdog int
+
+func (m hotdog)ServeHTTP(w http.ResponseWriter, req *http.Request)  {
+	err := req.ParseForm()
+	if err != nil{
+		log.Fatalln(err)
+	}
+	tpl.ExecuteTemplate(w, "index.gohtml", req.Form)
+}
+
+var tpl *template.Template
+
+func init()  {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func main() {
+	var d hotdog
+	http.ListenAndServe(":8080", d)
+}
