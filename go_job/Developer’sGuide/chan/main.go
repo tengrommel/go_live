614190760b2b40613a02e16065abaf8985a02f2d
tengrommel <wwwsousoucom@126.com>
2018-03-22 22:35:47 +0800

    Guide finish

---
+++ go_job/Developer’sGuide/chan/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+	for l := range c {
+		time.Sleep(time.Second)
+		go checkLink(l, c)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil{
+		fmt.Println(link, "might be down!")
+		c <- link
+		return
+	}
+	fmt.Println(link, "is up!")
+	c <- link
+}
