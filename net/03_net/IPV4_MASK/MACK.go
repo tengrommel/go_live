fe382d708b2a0cde0eb50c166ea5559869366bfb
tengrommel <wwwsousoucom@126.com>
2018-03-25 21:55:41 +0800

    net

---
+++ net/03_net/IPV4_MASK/MACK.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"net"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stdout, "Usage: %s dotted-ip-addr\n", os.Args[0])
+		os.Exit(1)
+	}
+	dotAddr := os.Args[1]
+
+	addr := net.ParseIP(dotAddr)
+	if addr == nil{
+		fmt.Println("Invalid address")
+		os.Exit(1)
+	}
+	mask := addr.DefaultMask()
+	network := addr.Mask(mask)
+	ones, bits := mask.Size()
+	fmt.Println("Address is ", addr.String(),
+		"\nDefault mask length is ", bits,
+		"\nLeading ones counts is ", ones,
+		"\nMask is (hex) ", mask.String(),
+		"\nNetwork is ", network.String())
+	os.Exit(1)
+}
+
